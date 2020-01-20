1710a51bbb4e0d2994f12e006811370ed572056a
Ramiro Rikkert <rrikkert@gmail.com>
2020-01-20 22:30:00 +0100

    Add stand alone tensorflow example.

--- tensorflow.js
+++ tensorflow.js
@@ -1,25 +1,73 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
+  const browser = await puppeteer.launch({
+    // Prevent canvas has been tainted by cross-origin data error
+    args: ["--disable-web-security"],
+    devtools: false,
+    headless: false
+  });
 
-    const browser = await puppeteer.launch({
-        headless: true,
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.volkskrant.nl/kijkverder/v/2020/dicht-bij-de-sterren-toen-dat-nog-kon~v84269/",
+    { waitUntil: "networkidle0" }
+  );
+
+  // Accept cookie wall
+  const btn = await page.$(".button.fjs-set-consent");
+  if (btn) {
+    await btn.click();
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+  }
+
+  await page.addScriptTag({
+    url: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"
+  });
+  await page.addScriptTag({
+    url: "https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"
+  });
+
+  const result = await page.evaluate(() => {
+    const canvas = document.createElement("canvas");
+
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.width = 800;
+    img.src =
+      "https://www.volkskrant.nl/kijkverder/v/2020/dicht-bij-de-sterren-toen-dat-nog-kon~v84269/media/869ad68b92e54348ad5c6f1af165199a.jpg";
+
+    console.log(img.textContent);
+    document.write(img.textContent);
+
+    img.addEventListener("load", e => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.style.border = "1px solid";
+
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      document.body.appendChild(canvas);
     });
 
-    const page = await browser.newPage();
+    // Load the model.
+    return cocoSsd
+      .load()
+      .then(model =>
+        // detect objects in the image.
+        model.detect(img)
+      )
+      .then(result => {
+        const { bbox } = result[0];
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
+        ctx.fillRect(bbox[0], bbox[1], bbox[2], bbox[3]);
+        return result;
+      });
+  });
 
-    await page.goto(`http://localhost:8002/tensorflow.html`);
+  console.log(result);
 
-    const result = await page.evaluate(() => {
-        const img = document.getElementById('img');
-
-        // Load the model.
-        return cocoSsd.load().then(model => model.detect(img))
-    });
-
-    console.log(result);
-
-    await page.close();
-
-    await browser.close();
+  //  await page.close();
+  //  await browser.close();
 })();
