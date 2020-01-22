82b0ca5a698facaf3e1f7aa5efd44a0e2c701ba8
Ramiro Rikkert <rrikkert@gmail.com>
2020-01-22 21:02:22 +0100

    SSL certificate example.

--- cert-expiration.js
+++ cert-expiration.js
@@ -1,20 +1,23 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-(async () => {
-  const siteUrl = 'https://www.linkit.nl/';
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+;(async () => {
+  const siteUrl = "https://www.acc.praxis.nl/";
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-  page.on('response', (resp) => {
-      const url = resp.url();
-      if (url === siteUrl) {
-          const secDetails = resp.securityDetails();
-          const ts = Math.floor((new Date()).getTime() / 1000);
-          console.log((Math.floor((secDetails.validTo() - ts) / 86400)), 'days to expire');
-      }
-  });
+  page.on('response', resp => {
+    const url = resp.url()
+    if (url === siteUrl) {
+      const secDetails = resp.securityDetails()
+      const ts = Math.floor(new Date().getTime() / 1000)
+      console.log(
+        Math.floor((secDetails.validTo() - ts) / 86400),
+        'days to expire'
+      )
+    }
+  })
 
-  await page.goto(siteUrl, { waitUntil: 'networkidle0' });
-  await page.close();
-  await browser.close();
-})();
+  await page.goto(siteUrl, { waitUntil: 'networkidle0' })
+  await page.close()
+  await browser.close()
+})()
