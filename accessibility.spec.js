6d79ef90b0ce99c1abd401be0b3e93e7a4d72bf0
Ramiro Rikkert <rrikkert@gmail.com>
2020-01-22 21:02:33 +0100

    Know-IT accessibility test.

--- accessibility.spec.js
+++ accessibility.spec.js
@@ -1,27 +1,29 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 // Inspiration: https://github.com/trentmwillis/devtools-protocol-demos/blob/master/testing-demos/accessibility.js
 
 beforeAll(async () => {
-  this.browser = await puppeteer.launch();
-});
+  this.browser = await puppeteer.launch()
+})
 
 afterAll(async () => {
-  await this.browser.close();
-});
+  await this.browser.close()
+})
 
-test('app is accessible and has no accessibility violations', async() => {
-  const page = await this.browser.newPage();
-  await page.goto('https://frontmania.com');
+test('app is accessible and has no accessibility violations', async () => {
+  const page = await this.browser.newPage()
+  await page.goto('http://www.know-it.nl')
 
-  await page.addScriptTag({ url: 'https://cdnjs.cloudflare.com/ajax/libs/axe-core/3.3.2/axe.min.js' });
-  const results = await page.evaluate(() => axe.run(document));
+  await page.addScriptTag({
+    url: 'https://cdnjs.cloudflare.com/ajax/libs/axe-core/3.3.2/axe.min.js'
+  })
+  const results = await page.evaluate(() => axe.run(document))
 
   if (results.violations.length > 0) {
-    console.log(`Found ${results.violations.length} accessibility violations`);
-    console.log(results.violations);
+    console.log(`Found ${results.violations.length} accessibility violations`)
+    console.log(results.violations)
   }
-  expect(results.violations.length).toBe(0);
+  expect(results.violations.length).toBe(0)
 
-  await page.close();
-});
+  await page.close()
+})
