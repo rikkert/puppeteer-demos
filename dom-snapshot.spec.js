c037460d402ef174de78d138993ec5575daefb54
Ramiro Rikkert <rrikkert@gmail.com>
2020-01-24 14:14:07 +0100

    Create Know IT dom snapshot test.

--- dom-snapshot.spec.js
+++ dom-snapshot.spec.js
@@ -1,15 +1,13 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-test('regression testing page dom snapshot', async() => {
-    const browser = await puppeteer.launch();
+test('regression testing Know IT page dom snapshot', async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-    const page = await browser.newPage();
+  await page.goto('http://www.know-it.nl/')
 
-    await page.goto('https://www.linkit.nl/');
+  expect(await page.content()).toMatchSnapshot()
 
-    expect(await page.content()).toMatchSnapshot();
-
-    await page.close();
-
-    await browser.close();
-});
+  await page.close()
+  await browser.close()
+})
